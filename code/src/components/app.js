41e7d8cd95d8dd338b9136e0873330ec0f797497
karinbellander <karinbellander@gmail.com>
2017-11-27 15:22:41 +0100

    first code

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -4,11 +4,79 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        Find me in src/app.js!
+
+  <div>
+      <div className="header">
+        <div className="header-container">
+
+          <div className="header-list">
+            <ul>
+              <li id="home">Home</li>
+              <li>Technology</li>
+              <li>Creativity</li>
+              <li>Entrepreneurship</li>
+              <li>Self</li>
+              <li>Culture</li>
+              <li>Digital design</li>
+              <li>Popular on Medium</li>
+              <li>Politics</li>
+              <li>More</li>
+            </ul>
+          </div>
+
+          <div className="button-header">
+            <a href="#" id="button-header">Get started</a>
+          </div>
+
+        </div>
       </div>
+
+      <div className="container">
+
+        <div className="container-intro">
+          <div className="intro-text">
+            <h1>Interesting ideas that set your mind in motion.</h1>
+            <p>Hear directly from the people who know it best. From tech to politics
+              to creativity and more — whatever your interest, we´ve got you covered.
+            </p>
+            <div className="button"><a href="#" id="getStarted">Get started</a> <a href="#"id="learnMore">Learn more</a></div>
+          </div>
+          <div className="intro-image">
+            <img src="/images/medium-image.png" alt="Illustration" id="intro-image"/>
+          </div>
+        </div>
+
+
+        <div className="container-content">
+
+          <div className="content">
+            <div className="content-image"><img src="/images/medium-image.png" alt="" className=""/></div>
+            <div className="content-text"></div>
+          </div>
+
+          <div className="content">
+            <div className="content-image"></div>
+            <div className="content-text"></div>
+          </div>
+
+          <div className="content">
+            <div className="content-image"></div>
+            <div className="content-text"></div>
+          </div>
+
+          <div className="content">
+            <div className="content-image"></div>
+            <div className="content-text"></div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
     )
   }
+
 
 }
 
